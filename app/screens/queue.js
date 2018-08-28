e6ecee8799d47c3a0474af798539ddef8deda028
joaovbalmeida <joaovbalmeida@gmail.com>
2018-08-28 17:29:20 -0300

    queue design

--- app/screens/queue.js
+++ app/screens/queue.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Text,
   Image,
+  TouchableHighlight,
 } from 'react-native';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -29,9 +30,15 @@ export class Queue extends Component {
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <FlatList
-          style={styles.playlists}
+          style={styles.flatlist}
           data={this.props.event.queue}
-          scrollEventThrottle={1}
+          ListHeaderComponent={() => (
+            <View style={styles.header}>
+              <TouchableHighlight style={styles.orderButton}>
+                <Text style={styles.orderText}>Pedir Música</Text>
+              </TouchableHighlight>
+            </View>
+          )}
           renderItem={({ item, index }) => (
             <Track
               order={(index + 1).toString()}
@@ -45,36 +52,46 @@ export class Queue extends Component {
           keyExtractor={item => item._id} // eslint-disable-line
           renderScrollComponent={() => (
             <ParallaxScrollView
-              backgroundColor="clear"
-              contentBackgroundColor="clear"
-              parallaxHeaderHeight={340}
-              stickyHeaderHeight={100}
+              backgroundColor="transparent"
+              contentBackgroundColor="#15191B"
+              parallaxHeaderHeight={380}
+              stickyHeaderHeight={120}
+              onChangeHeaderVisibility={visible => console.log(visible)}
               renderForeground={() => (
                 <View style={styles.foregroundContainer}>
                   <Image
                     style={styles.cover}
                     source={{ uri: queue[0].cover }}
                   />
+                  <Text style={styles.title}>
+                    {queue[0].name}
+                  </Text>
+                  <Text style={styles.artist}>
+                    {`${queue[0].artist} - ${queue[0].album}`}
+                  </Text>
+                  <Text style={styles.owner}>
+                    {queue[0].owner}
+                  </Text>
                 </View>
               )}
               renderBackground={() => (
-                <Image
-                  style={styles.backgroundImage}
-                  blurRadius={10}
-                  source={{ uri: queue[0].cover }}
-                  resizeMode="cover"
-                  opacity={0.1}
-                />
+                <View style={styles.backgroundContainer}>
+                  <Image
+                    style={styles.backgroundImage}
+                    blurRadius={18}
+                    source={{ uri: queue[0].cover }}
+                    resizeMode="cover"
+                    opacity={0.3}
+                  />
+                </View>
               )}
               renderStickyHeader={() => (
                 <View style={styles.stickyContainer}>
-                  <Text>{queue[0].name}</Text>
-                  <Text>
-                    {queue[0].artist}
-                    {''}
-                    -
-                    {''}
-                    {queue[0].album}
+                  <Text style={styles.title}>
+                    {queue[0].name}
+                  </Text>
+                  <Text style={styles.artist}>
+                    {`${queue[0].artist} - ${queue[0].album}`}
                   </Text>
                 </View>
               )}
@@ -96,14 +113,20 @@ Queue.propTypes = {
   }).isRequired,
 };
 
+const textStyles = {
+  color: 'white',
+  fontFamily: 'Raleway',
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#15191B',
   },
-  playlists: {
+  flatlist: {
     flex: 1,
     backgroundColor: '#15191B',
+    zIndex: 999999,
   },
   separator: {
     width: '100%',
@@ -111,25 +134,73 @@ const styles = StyleSheet.create({
     backgroundColor: '#15191B',
   },
   foregroundContainer: {
-    height: 340,
-    flex: 1,
+    height: 380,
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  stickyContainer: {
-    height: 100,
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-  backgroundImage: {
-    flex: 1,
-    alignItems: 'center',
   },
   cover: {
+    width: '55%',
     aspectRatio: 1,
     alignSelf: 'center',
+    marginTop: 10,
+  },
+  title: {
+    ...textStyles,
+    fontSize: 16,
+    fontWeight: '700',
+    paddingTop: 20,
+  },
+  artist: {
+    ...textStyles,
+    fontSize: 15,
+    fontWeight: '600',
+    paddingVertical: 5,
+  },
+  owner: {
+    fontFamily: 'Raleway',
+    fontSize: 15,
+    fontWeight: '600',
+    color: '#FF005A',
+  },
+  backgroundContainer: {
+    height: 380,
+    width: '100%',
+  },
+  backgroundImage: {
+    height: '100%',
+    alignItems: 'center',
+  },
+  header: {
+    height: 30,
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#15191B',
+    backgroundColor: '#0E1214',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  orderButton: {
+    position: 'absolute',
+    top: -15,
+    backgroundColor: '#FF005A',
+    height: 35,
+    width: 180,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  orderText: {
+    ...textStyles,
+    fontSize: 17,
+    fontWeight: '700',
+  },
+  stickyContainer: {
+    height: 120,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
   },
 });
 
